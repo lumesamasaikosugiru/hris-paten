13b8837f9ac48b513756c72b1af815702133001b
lumesamasaikosugiru <lumetrixsynf@gmail.com>
2026-01-09 09:24:57 +0700

    fill seeder class=SkillSeeder

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SkillSeeder extends Seeder
 {
@@ -13,5 +14,31 @@ class SkillSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('skills')->insert([
+            [
+                'name' => 'Tersertifikasi Bahasa Inggris',
+                'type' => 'soft',
+            ],
+            [
+                'name' => 'Tersertifikasi Jepang',
+                'type' => 'soft',
+            ],
+            [
+                'name' => 'Desainer Grafis',
+                'type' => 'technical',
+            ],
+            [
+                'name' => 'Excel,Word,PowerPoint',
+                'type' => 'technical',
+            ],
+            [
+                'name' => 'Las Listrik',
+                'type' => 'technical',
+            ],
+            [
+                'name' => 'Membubut',
+                'type' => 'technical',
+            ],
+        ]);
     }
 }
